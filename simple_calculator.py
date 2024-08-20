f378c0bedd86ff8e371c7a20d511a162927b3b58
Kilu Chandran <kiluchandran@gmail.com>
2024-08-20 13:55:15 +0530

    Add label to the window

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,7 +2,14 @@ from tkinter import *
 
 window=Tk()
 window.title("Simple Calculator")
-window.geometry("400x300")
+window.geometry("500x500")
+display=StringVar()
+display.set("0")
+
+display_label = Label(window, textvariable=display, font=("Arial", 24), anchor="e", background="white",width=28,height=1)
+display_label.grid()
+
+
 
 
 
