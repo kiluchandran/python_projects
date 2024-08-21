d4417b7f3c1f02da722a501062eea1e00279200c
Kilu Chandran <kiluchandran@gmail.com>
2024-08-21 10:52:55 +0530

    Create function to call  when a button is clicked using lambda

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,67 +1,72 @@
 from tkinter import *
 
-#Create main window
-window=Tk()
+def button_clicked(text):
+    display.set(text)
+
+# Create main window
+window = Tk()
 window.title("Simple Calculator")
 window.geometry("400x400")
 
-#Create a variable to store the current display value
-display=StringVar()
+# Create a variable to store the current display value
+display = StringVar()
 display.set("0")
 
-#Create display label
+# Create display label
 display_label = Label(window, textvariable=display,height=3,width=40,bg="lightblue",anchor="e")
 display_label.grid(row=0,column=0,columnspan=11)
 
-#create button
-button1=Button(window,text="1",padx=30,pady=20)
-button1.grid(row=1,column=0)
 
-button2=Button(window,text="2",padx=30,pady=20)
-button2.grid(row=1,column=1)
+# Create buttons
+button_1 = Button(window,text="1",padx=30,pady=20,command=lambda :button_clicked("1"))
+button_1.grid(row=1,column=0)
+
+button_2 = Button(window,text="2",padx=30,pady=20,command=lambda :button_clicked("2"))
+button_2.grid(row=1,column=1)
 
 
-button3=Button(window,text="3",padx=30,pady=20)
-button3.grid(row=1,column=2)
+button_3 = Button(window,text="3",padx=30,pady=20,command=lambda :button_clicked("3"))
+button_3.grid(row=1,column=2)
 
-button4=Button(window,text="+",padx=30,pady=20)
-button4.grid(row=1,column=3)
+plus_button = Button(window,text="+",padx=30,pady=20,command=lambda :button_clicked("+"))
+plus_button.grid(row=1,column=3)
 
-button5=Button(window,text="4",padx=30,pady=20)
-button5.grid(row=2,column=0)
+button_4 = Button(window,text="4",padx=30,pady=20,command=lambda :button_clicked("4"))
+button_4.grid(row=2,column=0)
 
-button6=Button(window,text="5",padx=30,pady=20)
-button6.grid(row=2,column=1)
+button_5 = Button(window,text="5",padx=30,pady=20,command=lambda :button_clicked("5"))
+button_5.grid(row=2,column=1)
 
-button7=Button(window,text="6",padx=30,pady=20)
-button7.grid(row=2,column=2)
+button_6 = Button(window,text="6",padx=30,pady=20,command=lambda :button_clicked("6"))
 
-button8=Button(window,text="-",padx=30,pady=20)
-button8.grid(row=2,column=3)
+button_6.grid(row=2,column=2)
 
-button9=Button(window,text="7",padx=30,pady=20)
-button9.grid(row=3,column=0)
+minus_button = Button(window,text="-",padx=30,pady=20,command=lambda :button_clicked("-"))
+minus_button.grid(row=2,column=3)
 
-button10=Button(window,text="8",padx=30,pady=20)
-button10.grid(row=3,column=1)
+button_7 = Button(window,text="7",padx=30,pady=20,command=lambda :button_clicked("7"))
+button_7.grid(row=3,column=0)
 
-button11=Button(window,text="9",padx=30,pady=20)
-button11.grid(row=3,column=2)
+button_8 = Button(window,text="8",padx=30,pady=20,command=lambda :button_clicked("8"))
+button_8.grid(row=3,column=1)
 
-button12=Button(window,text="*",padx=30,pady=20)
-button12.grid(row=3,column=3)
+button_9 = Button(window,text="9",padx=30,pady=20,command=lambda :button_clicked("9"))
+button_9.grid(row=3,column=2)
 
-button13=Button(window,text="0",padx=30,pady=20)
-button13.grid(row=4,column=0)
+mul_button = Button(window,text="*",padx=30,pady=20,command=lambda :button_clicked("*"))
+mul_button.grid(row=3,column=3)
 
-button14=Button(window,text="clear",padx=20,pady=20)
-button14.grid(row=4,column=1)
+zero_button = Button(window,text="0",padx=30,pady=20,command=lambda :button_clicked("0"))
+zero_button.grid(row=4,column=0)
 
-button15=Button(window,text=".",padx=30,pady=20)
-button15.grid(row=4,column=2)
+clear_button = Button(window,text="clear",padx=20,pady=20,command=lambda :button_clicked("0"))
+clear_button.grid(row=4,column=1)
 
-button16=Button(window,text="/",padx=30,pady=20)
-button16.grid(row=4,column=3)
+dot_button = Button(window,text=".",padx=30,pady=20,command=lambda :button_clicked("."))
+dot_button.grid(row=4,column=2)
+
+div_button = Button(window,text="/",padx=30,pady=20,command=lambda :button_clicked("/"))
+div_button.grid(row=4,column=3)
 
 
 
