c9786dd00d6a3a27c99ae53c5840e164f832eae1
Kilu Chandran <kiluchandran@gmail.com>
2024-08-22 11:24:15 +0530

    Create and define calculate_display function

--- simple_calculator.py
+++ simple_calculator.py
@@ -11,6 +11,14 @@ def button_clicked(value):
 def clear_display():
     display.set("0")
 
+def calculate_display():
+    try:
+        result =eval(display.get())
+        display.set(result)
+    except:
+        display.set("error")
+
+
 
 # Create main window
 window = Tk()
@@ -27,57 +35,59 @@ display_label.grid(row=0,column=0,columnspan=11)
 
 
 # Create buttons
-button_1 = Button(window,text="1",padx=30,pady=20,command=lambda :button_clicked("1"))
+button_1 = Button(window,text="1",padx=30,pady=20,command=lambda t="1" :button_clicked(t) if t != "=" else calculate_display())
 button_1.grid(row=1,column=0)
 
-button_2 = Button(window,text="2",padx=30,pady=20,command=lambda :button_clicked("2"))
+button_2 = Button(window,text="2",padx=30,pady=20,command=lambda t="2" :button_clicked(t) if t != "=" else calculate_display())
 button_2.grid(row=1,column=1)
 
 
-button_3 = Button(window,text="3",padx=30,pady=20,command=lambda :button_clicked("3"))
+button_3 = Button(window,text="3",padx=30,pady=20,command=lambda t="3" :button_clicked(t) if t != "=" else calculate_display() )
 button_3.grid(row=1,column=2)
 
-plus_button = Button(window,text="+",padx=30,pady=20,command=lambda :button_clicked("+"))
+plus_button = Button(window,text="+",padx=30,pady=20,command=lambda t="+":button_clicked(t) if t != "=" else calculate_display())
 plus_button.grid(row=1,column=3)
 
-button_4 = Button(window,text="4",padx=30,pady=20,command=lambda :button_clicked("4"))
+button_4 = Button(window,text="4",padx=30,pady=20,command=lambda t="4": button_clicked(t) if t != "=" else calculate_display())
 button_4.grid(row=2,column=0)
 
-button_5 = Button(window,text="5",padx=30,pady=20,command=lambda :button_clicked("5"))
+button_5 = Button(window,text="5",padx=30,pady=20,command=lambda t="5": button_clicked(t) if t != "=" else calculate_display())
 button_5.grid(row=2,column=1)
 
-button_6 = Button(window,text="6",padx=30,pady=20,command=lambda :button_clicked("6"))
+button_6 = Button(window,text="6",padx=30,pady=20,command=lambda t="6": button_clicked(t) if t != "=" else calculate_display())
 
 button_6.grid(row=2,column=2)
 
-minus_button = Button(window,text="-",padx=30,pady=20,command=lambda :button_clicked("-"))
+minus_button = Button(window,text="-",padx=30,pady=20,command=lambda t="-": button_clicked(t) if t != "=" else calculate_display())
 minus_button.grid(row=2,column=3)
 
-button_7 = Button(window,text="7",padx=30,pady=20,command=lambda :button_clicked("7"))
+button_7 = Button(window,text="7",padx=30,pady=20,command=lambda t="7": button_clicked(t) if t != "=" else calculate_display())
 button_7.grid(row=3,column=0)
 
-button_8 = Button(window,text="8",padx=30,pady=20,command=lambda :button_clicked("8"))
+button_8 = Button(window,text="8",padx=30,pady=20,command=lambda t="8": button_clicked(t) if t != "=" else calculate_display())
 button_8.grid(row=3,column=1)
 
-button_9 = Button(window,text="9",padx=30,pady=20,command=lambda :button_clicked("9"))
+button_9 = Button(window,text="9",padx=30,pady=20,command=lambda t="9": button_clicked(t) if t != "=" else calculate_display())
 button_9.grid(row=3,column=2)
 
-mul_button = Button(window,text="*",padx=30,pady=20,command=lambda :button_clicked("*"))
+mul_button = Button(window,text="*",padx=30,pady=20,command=lambda t="*": button_clicked(t) if t != "=" else calculate_display())
 mul_button.grid(row=3,column=3)
 
-zero_button = Button(window,text="0",padx=30,pady=20,command=lambda :button_clicked("0"))
+zero_button = Button(window,text="0",padx=30,pady=20,command=lambda t="0": button_clicked(t) if t != "=" else calculate_display())
 zero_button.grid(row=4,column=0)
 
 clear_button = Button(window,text="clear",padx=20,pady=20,command=clear_display)
 clear_button.grid(row=4,column=1)
 
 
-dot_button = Button(window,text=".",padx=30,pady=20,command=lambda :button_clicked("."))
+dot_button = Button(window,text=".",padx=30,pady=20,command=lambda t=".": button_clicked(t) if t != "=" else calculate_display())
 dot_button.grid(row=4,column=2)
 
-div_button = Button(window,text="/",padx=30,pady=20,command=lambda :button_clicked("/"))
+div_button = Button(window,text="/",padx=30,pady=20,command=lambda t="/": button_clicked(t) if t != "=" else calculate_display())
 div_button.grid(row=4,column=3)
 
+equal_button=Button(window,text="=",padx=30,pady=20,command=lambda t="=": button_clicked(t) if t != "=" else calculate_display())
+equal_button.grid(row=5,column=0)
 
 
 
