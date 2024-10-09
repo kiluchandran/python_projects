f1aaa8837fce11660c3f86826fee6f0a5d5fe5b5
Kilu Chandran <kiluchandran@gmail.com>
2024-10-09 14:06:13 +0530

    Player1 takes the second chance to play the game

--- 21_number_game.py
+++ 21_number_game.py
@@ -3,8 +3,16 @@ def check(xyz):
     while i < len(xyz):
         if xyz[i] - xyz[i-1] != 1:
             return False
-        i =i + 1
+        i = i + 1
     return True
+
+
+def nearest_multiple(num):
+    if num >= 4:
+        near = num + (4 - (num % 4))
+    else:
+        near = 4
+    return near
 
 
 def lose():
@@ -28,7 +36,7 @@ def start_game():
                     print("Your turn")
                     print("How many numbers do you want to enter?")
                     inp = int(input("<"))
-                    if 0<inp<=3:
+                    if 0 < inp <= 3:
                         comp = 4 - inp
                     else:
                         print("wrong choice. you disqualified from the game")
@@ -36,16 +44,16 @@ def start_game():
 
                     print("Your turn")
                     print("Enter your values")
-                    i,j = 1,1
+                    i, j = 1, 1
 
                     while i <= inp:
                         a = int(input("<"))
                         xyz.append(a)
-                        i =i + 1
+                        i = i + 1
 
                     last = xyz[-1]
 
-                    if check(xyz) == True :
+                    if check(xyz):
                         if last == 21:
                             lose()
                         else:
@@ -59,24 +67,50 @@ def start_game():
                     else:
                         print("you did not input consecutive integers")
                         lose()
+        elif chance == "S":
+            xyz = []
+            last = 0
+            comp = 1
+            while last < 20:
+                j = 1
+                while j <= comp:
+                    xyz.append(j + last)
+                    j = j + 1
+                print("Order of inputs after computer's turn :")
+                print(xyz)
+                if xyz[-1] == 20:
+                    lose()
+                else:
+                    print("Your turn")
+                    inp = int(input("How many numbers do you want to enter?"))
+                    i = 1
+                    print("Enter your values")
+                    while i <= inp:
+                        a = int(input(">"))
+                        xyz.append(a)
+                        i = i + 1
+                    last = xyz[-1]
+                    if check(xyz):
+                        near = nearest_multiple(last)
+                        comp = near - last
+                        if comp == 4:
+                            comp = 3
+                        else:
+                            comp = comp
 
+                    else:
+                        print("you did not enter consecutive integers")
+                        lose()
+            print("Congratulations")
+            print("You won")
+            exit()
 
-        if chance == "S":
-
-
-
-
-
-
-
-
-
-
-
+        else:
+            print("wrong choice")
 
 
 game = True
-while game == True:
+while game:
     print("Player 2 is computer \n")
     print("Do you want to play the 21 number game?(yes/no)")
     ans = input("<")
